6009e2cf7cad29844e8add462b44abe1659c7627
st-182 <31894656+st-182@users.noreply.github.com>
2022-05-28 18:44:31 +0300

    start of the cource

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Page for finding musical events.</p>
+      <p>Version 1.0</p>
+    </div>
+  );
+}
